372864157f8713b70b9b4569e8d076e904612518
Oleksandr Korotych <korotych.o@soft2bet.com>
2023-11-21 12:13:01 +0200

    code review

--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -124,7 +124,7 @@ func (h *basicAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// 	w.Header().Set("WWW-Authenticate", "Basic realm=\"metrics\"")
 	// 	http.Error(w, "Invalid username or password", http.StatusUnauthorized)
 	// 	return
-		continue
+		break
 	}
 	h.handler(w, r)
 }
