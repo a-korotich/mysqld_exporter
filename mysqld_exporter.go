2453d090fb5d7358ccf88857711470380c9d2aa9
Oleksandr Korotych <korotych.o@soft2bet.com>
2024-12-12 10:28:08 +0200

    Add partitions to remove metrics

--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -138,6 +138,7 @@ var _ promhttp.Logger = &errLogger{}
 var scrapers = map[collector.Scraper]bool{
 	dba.ScrapeStatColumnCapacityColumns{}:                 false,
 	dba.ScrapeUsingPartitions{}:                           false,
+	dba.ScrapePartitionsToRemove{}:						   false,
 	dba.ScrapeOpenTables{}:                                false,
 	pcl.ScrapeGlobalStatus{}:                              false,
 	collector.ScrapeGlobalStatus{}:                        false,
